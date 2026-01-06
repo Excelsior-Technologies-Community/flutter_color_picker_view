0eb8ce1774f327bf0f9433d4daa1af8d0c0c1111
chetan9357 <chetansolanki9357@gmail.com>
2026-01-06 15:30:12 +0530

    .

--- lib/demo/color_picker_demo_screen.dart
+++ lib/demo/color_picker_demo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_color_picker_view/features/color_picker/view/color_picker_view.dart';
 import 'package:flutter_color_picker_view/features/color_picker/widgets/color_picker_dialog.dart';
 
 class ColorPickerDemoScreen extends StatefulWidget {
@@ -24,20 +25,29 @@ class _ColorPickerDemoScreenState extends State<ColorPickerDemoScreen> {
       ),
       body: Column(
         children: [
-          Expanded(child: SizedBox(height: 20)),
-          ElevatedButton(
-            onPressed: () {
-              showColorPickerDialog(
-                context: context,
-                initialColor: selectedColor,
-                onColorSelected: (color) {
-                  setState(() => selectedColor = color);
-                },
-              );
-            },
-            child: const Text('Pick Color'),
+          Expanded(
+            child: ColorPickerView(
+              onColorSelected: (color){
+                setState(() {
+                  color = color;
+                });
+              },
+              initialColor: selectedColor,
+            ),
           ),
-          Expanded(child: SizedBox(height: 20)),
+          // ElevatedButton(
+          //   onPressed: () {
+          //     showColorPickerDialog(
+          //       context: context,
+          //       initialColor: selectedColor,
+          //       onColorSelected: (color) {
+          //         setState(() => selectedColor = color);
+          //       },
+          //     );
+          //   },
+          //   child: const Text('Pick Color'),
+          // ),
+          // Expanded(child: SizedBox(height: 20)),
           Container(
             height: 80,
             color: selectedColor,
